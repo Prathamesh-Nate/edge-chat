f324222e78aa3cbebaf57f4edb402da22347257a
Rama Vats <guy55222@gmail.com>
2023-03-22 01:15:10 +0530

    error page added

--- components/WrongNetworkMessage.js
+++ components/WrongNetworkMessage.js
@@ -1,11 +1,11 @@
 const WrongNetworkMessage = () => (
-  <div className="formContainer">
-    <div className="formWrapper">
+  <div className="card">
+    <div className="mockup-code">
       {/* Prompt to change network to Rinkeby */}
-      <div>----------------------------------------</div>
-      <div>Please connect to the Goerli Testnet</div>
-      <div>and reload the page</div>
-      <div>----------------------------------------</div>
+      <pre data-prefix="1"><code>Connecting to Edgeware Beresheet...</code></pre>
+      <pre data-prefix="2"><code>Error! Retrying...</code></pre>
+      <pre data-prefix="3" class="bg-warning text-warning-content"><code>Please connect to the Edgeware Beresheet</code></pre>
+      <pre data-prefix="4" class="bg-warning text-warning-content"><code>and reload the page</code></pre>
     </div>
   </div>
 );
