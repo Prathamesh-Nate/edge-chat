215f21405196118f3035499194e2976d3c536a64
Prathamesh-Nate <prathameshnate.sprints@gmail.com>
2023-03-25 13:39:34 +0530

    Add default UI on loading chat

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -2,16 +2,21 @@ import React, { useContext } from "react";
 import { ChatContext } from "../context/ChatContext";
 import Message from "./Message";
 import Input from "./Input";
+import AnimatedLetters from './AnimatedLetters/index';
 
 const Chat = () => {
   const { selectedUserName } = useContext(ChatContext);
-  return (
+  return ( 
     <div className="chat-ui">
       <div className="chat-info">
         <span>{selectedUserName}</span>
       </div>
-      <Message />
-      <Input />
+      {selectedUserName !=='' ?
+      <>
+        <Message />
+        <Input />
+      </> :
+      <AnimatedLetters />}
     </div>
   );
 };
